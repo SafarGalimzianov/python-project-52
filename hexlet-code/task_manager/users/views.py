17612070cf18ff53b649663ab6ad1be8a27f62da
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-02-08 13:55:54 +0500

    0.9.1: fix users registration

--- hexlet-code/task_manager/users/views.py
+++ hexlet-code/task_manager/users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, reverse
 from django.views.generic import TemplateView, ListView, UpdateView, DeleteView, CreateView
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from task_manager.users.models import User
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 # Create your views here.
 
@@ -34,7 +35,6 @@ class UserCreatePageView(CreateView):
     model = User
     fields = ['username', 'email', 'password']
     labels = ['Username', 'Email', 'Password']
-    # form_class = UserCreationForm
     template_name = 'create.html'
     success_url = reverse_lazy('users')
 
@@ -42,6 +42,25 @@ class UserCreatePageView(CreateView):
         context = super().get_context_data(**kwargs)
         context['form_fields'] = zip(self.fields, self.labels)
         return context
+    
+    def form_invalid(self, form):
+        flash_messages = {
+            'email_invalid': 'Please enter a valid email',
+            'email_exists': 'Please enter a different email',
+            'username_exists': 'Please use a different username',
+        }
+        if 'email' in form.errors:
+            for error in form.errors['email']:
+                if 'Enter a valid email address' in error:
+                    messages.error(self.request, flash_messages['email_invalid'], extra_tags='warning')
+                elif 'exists' in error:
+                    messages.error(self.request, flash_messages['email_exists'], extra_tags='warning')
+        if 'username' in form.errors:
+            for error in form.errors['username']:
+                if 'already exists' in error:
+                    messages.error(self.request, flash_messages['username_exists'], extra_tags='warning')
+
+        return super().form_invalid(form)
 
 class UserUpdatePageView(UpdateView):
     model = User
