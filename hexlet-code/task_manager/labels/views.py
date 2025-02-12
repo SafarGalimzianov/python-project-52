5d1afc304026fbcc78f3de05384ca3f5fa04e524
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-02-13 00:25:54 +0500

    0.11.2: update labels views

--- hexlet-code/task_manager/labels/views.py
+++ hexlet-code/task_manager/labels/views.py
@@ -5,20 +5,27 @@ from django.views.generic.edit import FormMixin
 from task_manager.labels.models import Label
 from task_manager.labels.forms import LabelForm
 
-class LabelPageView(FormMixin, ListView):
-    template_name = 'index_labels.html'  
+class LabelFormMixin(FormMixin):
     model = Label
     form_class = LabelForm
-    context_object_name = 'table_content'
+    context_extra = {}
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Labels'
         context['form'] = self.get_form()
-        context['table_headers'] = ['ID', 'Label', 'Actions']
+        context.update(self.context_extra)
         return context
 
+class LabelPageView(LabelFormMixin, ListView):
+    template_name = 'index_labels.html'
+    context_object_name = 'table_content'
+    context_extra = {
+            'title': 'Labels',
+            'table_headers': ['ID', 'Label', 'Actions'],
+        }
+
 class LabelCreatePageView(CreateView):
+    template_name = 'create.html'
     model = Label
     form_class = LabelForm
     success_url = reverse_lazy('labels')
@@ -38,16 +45,9 @@ class LabelCreatePageView(CreateView):
                 messages.error(self.request, f"Error in {field}: {error}")
         return super().form_invalid(form)
 
-class LabelUpdatePageView(UpdateView):
+class LabelUpdatePageView(LabelFormMixin, UpdateView):
     template_name = 'update.html'
-    model = Label
-    form_class = LabelForm
     success_url = reverse_lazy('labels')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = self.get_form()
-        return context
 
 class LabelDeletePageView(DeleteView):
     model = Label
