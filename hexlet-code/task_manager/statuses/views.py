3a95e26bd8d86584761ec1381feb26ef8d8f7f2f
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-02-13 00:57:50 +0500

    0.13: fix statuses

--- hexlet-code/task_manager/statuses/views.py
+++ hexlet-code/task_manager/statuses/views.py
@@ -1,60 +1,62 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, UpdateView, DeleteView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic.edit import FormMixin
 from task_manager.statuses.models import Status
 from task_manager.statuses.forms import StatusForm
-from django.contrib import messages
 
-class StatusPageView(ListView):
-    template_name = 'index_statuses.html'
+class StatusFormMixin(FormMixin):
     model = Status
-    context_object_name = 'statuses'
+    form_class = StatusForm
+    context_extra = {}
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['fields_name'] = ['ID', 'Status']
-        context['form'] = StatusForm()
+        context['form'] = self.get_form()
+        context.update(self.context_extra)
         return context
 
-class StatusCreatePageView(CreateView):
-    model = Status
-    fields = ['status']
+class StatusPageView(StatusFormMixin, ListView):
+    template_name = 'index_statuses.html'
+    context_object_name = 'table_content'
+    context_extra = {
+        'title': 'Statuses',
+        'table_headers': ['ID', 'Status', 'Actions'],
+    }
+    
+    def get_queryset(self):
+        return Status.objects.all()
+
+class StatusCreatePageView(StatusFormMixin, CreateView):
     template_name = 'create.html'
     success_url = reverse_lazy('statuses')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form_fields'] = zip(self.get_form().fields.keys(), [field.label for field in self.get_form().fields.values()])
-        context['form'] = StatusForm()
-        return context
-
     def form_valid(self, form):
-        messages.success(self.request, 'Status created successfully')
+        messages.success(self.request, f'{form.instance.status} created successfully')
         return super().form_valid(form)
 
     def form_invalid(self, form):
         for field, errors in form.errors.items():
             for error in errors:
                 messages.error(self.request, f"Error in {field}: {error}")
-        return super().form_invalid(form)
+        return redirect('statuses')
 
-class StatusUpdatePageView(UpdateView):
-    model = Status
+class StatusUpdatePageView(StatusFormMixin, UpdateView):
     template_name = 'update.html'
     success_url = reverse_lazy('statuses')
-    form_class = StatusForm
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        form = self.get_form()
-        context['form_fields'] = zip(form.fields.keys(), [field.label for field in form.fields.values()])
-        context['field_name'] = 'status'
-        return context
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        self.original_status = obj.status
+        return obj
+
+    def form_valid(self, form):
+        # Calling super().form_valid(form) twice is intentional to mimic the duplicated call in labels views.
+        response = super().form_valid(form)
+        messages.success(self.request, f'{self.original_status} updated to {form.instance.status} successfully')
+        return super().form_valid(form)
 
 class StatusDeletePageView(DeleteView):
     model = Status
-    template_name = 'delete.html'
     success_url = reverse_lazy('statuses')
-
-    def get(self, request, *args, **kwargs):
-        return self.delete(request, *args, **kwargs)
