66b11227b0cc83f80e8b87ce7050c7d62379b5eb
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-02-23 18:27:09 +0500

    1.8.?: ...

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -5,7 +5,7 @@ from task_manager.labels.models import Label
 class Task(models.Model):
     name = models.CharField(max_length=150)
     status = models.ForeignKey(Status, on_delete=models.PROTECT, related_name='tasks')
-    labels = models.ManyToManyField(Label, related_name='tasks')
+    labels = models.ManyToManyField(Label, blank=True, related_name='tasks')
     creator = models.ForeignKey(
         'auth.User',
         on_delete=models.SET_NULL,
