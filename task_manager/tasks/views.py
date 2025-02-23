ab93c96e700d3e17864fcdc8cf229fb39e31ffcd
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-02-23 14:58:45 +0500

    1.8.?: ...

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -9,7 +9,7 @@ from task_manager.tasks.mixins import TaskFormMixin
 from task_manager.tasks.filters import TaskFilter
 
 class TaskPageView(LoginRequiredMixin, FilterView, TaskFormMixin, ListView):
-    template_name = 'tasks/index_tasks.html'
+    template_name = 'tasks/test.html'
     filterset_class = TaskFilter
     context_extra = {
         'title': 'Tasks',
@@ -19,7 +19,7 @@ class TaskPageView(LoginRequiredMixin, FilterView, TaskFormMixin, ListView):
     }
 
 class TaskCreatePageView(LoginRequiredMixin, TaskFormMixin, CreateView):
-    template_name = 'create.html'
+    template_name = 'tasks/create_tasks.html'
     success_url = reverse_lazy('tasks')
 
     def form_invalid(self, form):
@@ -32,7 +32,7 @@ class TaskCreatePageView(LoginRequiredMixin, TaskFormMixin, CreateView):
         form.instance.creator = self.request.user
         if not self.request.user.is_staff:
             form.instance.responsible = self.request.user
-        messages.success(self.request, 'Task created successfully')
+        messages.success(self.request, 'Задача успешно создана')
         return super().form_valid(form)
 
 class TaskUpdatePageView(LoginRequiredMixin, TaskFormMixin, UpdateView):
@@ -45,16 +45,31 @@ class TaskUpdatePageView(LoginRequiredMixin, TaskFormMixin, UpdateView):
         return obj
 
     def form_valid(self, form):
+        messages.success(self.request, 'Задача успешно изменена')
+        '''
         messages.success(
             self.request,
             f'{self.original_description} updated to {form.instance.description} successfully'
         )
+        '''
         return super().form_valid(form)
 
 class TaskDeletePageView(LoginRequiredMixin, DeleteView):
     model = Task
     success_url = reverse_lazy('tasks')
-    
+
+    template_name = 'delete.html'
+    context_extra = {
+        'header': 'Statuses',
+        'fields_names': ['ID', 'name'],
+    }
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.success(self.request, 'Задача успешно удалена', extra_tags='.alert')
+        return super().dispatch(request, *args, **kwargs)
+
+    '''
     def get(self, request, *args, **kwargs):
         """Override get to handle deletion without template"""
         return self.delete(request, *args, **kwargs)
+    '''
