9129df66888c8df6d41cc37f9a264a0ef66e2373
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-02-26 22:45:28 +0500

    1.8.?: ...

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -5,3 +5,7 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'status', 'labels', 'creator', 'executor', 'description']
+
+    def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['executor'].label_from_instance = lambda obj: f"{obj.first_name} {obj.last_name}"
