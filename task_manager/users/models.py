40c7931d6b9aa85479f154fb7429904e710db7e1
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-02-27 22:08:58 +0500

    1.8.?: ...

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import User as DjangoUser
 class User(DjangoUser):
     class Meta:
         proxy = True
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
