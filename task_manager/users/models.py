e9d819b84d23c30f406587244d45e1b8a4500746
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-03-01 10:47:55 +0500

    1.9.1: replace DjangoUser with AbstractUser for more flexibility

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,8 +1,6 @@
-from django.contrib.auth.models import User as DjangoUser
+from django.contrib.auth.models import AbstractUser
 
-class User(DjangoUser):
-    class Meta:
-        proxy = True
+class User(AbstractUser):
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        return f"{self.first_name} {self.last_name}"
