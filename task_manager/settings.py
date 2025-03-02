4d72b0496dec4331d91381fcd18771d8fca4563a
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-03-02 14:35:52 +0500

    1.9.5: still use User model

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -160,7 +160,7 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = 'users.User'
+# AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,14 +1,6 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User as DjangoUser
 
-class User(AbstractUser):
-    '''
-    id = models.AutoField(primary_key=True, default=1000)
-    username = models.CharField(unique=True, default=1000)
-    first_name = models.CharField(default=1000)
-    last_name = models.CharField(default=1000)
-    password = models.CharField(default=1000)
-    '''
+class User(DjangoUser):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
