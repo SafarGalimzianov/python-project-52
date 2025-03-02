5ce46fa0c112d6d790f9ff1ed68837bd7d22c186
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-03-02 22:57:50 +0500

    1.15.4: fix linter errors

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -4,8 +4,13 @@ from task_manager.labels import views
 urlpatterns = [
     path('', views.LabelPageView.as_view(), name='labels'),
     path('create/', views.LabelCreatePageView.as_view(), name='label_create'),
-    path('<int:pk>/update/', views.LabelUpdatePageView.as_view(), \
-         name='label_update'),
-    path('<int:pk>/delete/', views.LabelDeletePageView.as_view(), \
-         name='label_delete'),
+    path(
+        '<int:pk>/update/',
+        views.LabelUpdatePageView.as_view(),
+        name='label_update'),
+    path(
+        '<int:pk>/delete/',
+        views.LabelDeletePageView.as_view(),
+        name='label_delete'
+     ),
 ]
